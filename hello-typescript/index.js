241ff5e358aff1a8016e8c906327a7b44a93b64e
Elek <elek@innotech.me>
2021-01-06 11:12:35 +0800

    intial commit

---
+++ hello-typescript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var msg = '123';
+function sayHello(something) {
+    console.log(something);
+}
+sayHello(msg);
---
+++ hello-typescript/index.ts
@@ -0,0 +1,9 @@
+const msg ='123'
+function sayHello(something: string): void {
+    console.log(something);
+}
+
+sayHello(msg)
+
+const message = 'Hello world'
+console.log(message.toUpperCase());
